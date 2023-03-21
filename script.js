3c8a59ff62a4949b2a7c79cd13e6911d080866f7
Hinali2010 <127829014+Hinali2010@users.noreply.github.com>
2023-03-21 07:38:20 +0000

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello!');
+var highestBox = 0;
+$('.event-topic').each(function () {
+  if ($(this).height() > highestBox) {
+    highestBox = $(this).height();
+  }
+});
+$('.event-topic').height(highestBox);
